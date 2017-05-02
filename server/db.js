9be9ee642e8deef83a4f74737d1b8c972fa66090
Francesco Lanciana <frankface@192-168-1-11.tpgi.com.au>
2017-05-02 20:48:21 +1000

    Fixing heroku db

--- server/db.js
+++ server/db.js
@@ -11,8 +11,14 @@ var state = {
 
 exports.connect = function(done) {
   console.log(process.env.JAWSDB_URL);
-  state.pool = mysql.createPool(process.env.JAWSDB_URL);
-
+  state.pool = mysql.createPool({
+    connectionLimit : 100,
+    host            : 'o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user            : 'budoi4pbm8nt7exx',
+    password        : 'zbxgrkejwu21yd0d',
+    database        : 'xon579hsknob2lyt'
+  });
+  console.log(state.pool);
   state.mode = 'normal';
   done()
 }
